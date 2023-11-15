f72349448eacea88263720d290a2b670e80cf312
Lucas Beauloi <l.beauloi@gmail.com>
2023-11-15 14:07:10 +0100

    finish

--- request.sql
+++ request.sql
@@ -27,11 +27,22 @@ WHERE students.school = (
 	WHERE nom = "Addy")
 
 -- 6 Affiche uniquement les prénoms des étudiants, par ordre inverse à l’alphabet (DESC). Ensuite, la même chose mais en limitant les résultats à 2.
+SELECT prenom
+FROM students
+ORDER BY prenom DESC; 
+
+SELECT prenom
+FROM students
+ORDER BY prenom DESC LIMIT 2;
 
 -- 7 Ajoute Ginette Dalor, née le 01/01/1930 et affecte-la à Bruxelles, toujours en SQL.
+INSERT INTO `students`(`idStudent`,`nom`, `prenom`, `datenaissance`, `genre`, `school`) VALUES ('31','Dalor','Ginette','1930-01-01','F','1')
 
 -- 8 Modifie Ginette (toujours en SQL) et change son sexe et son prénom en “Omer”.
+UPDATE students SET prenom="Omer", genre="M" WHERE idStudent=31;
 
 -- 9 Supprimer la personne dont l’ID est 3.
+DELETE FROM students WHERE idStudent=3
 
 -- 10 Modifier le contenu de la colonne School de sorte que "1" soit remplacé par "Liege" et "2" soit remplacé par "Gent". (attention au type de la colonne !)
+UPDATE students SET school="Liège" WHERE school=2
